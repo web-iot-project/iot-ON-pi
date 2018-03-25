e00306ea3413a8be454b19d6d6c58bc98a10bb19
Chris <chriswfoster@gmail.com>
2018-03-24 21:14:37 -0500

    Test new motor

--- server/iotIndex.js
+++ server/iotIndex.js
@@ -1,35 +1,36 @@
 var Gpio = require('onoff').Gpio
-var LED = new Gpio(4, 'out') //use GPIO pin 4, change the 4 to respecify the pin number
-let blinkInterval // = setInterval(blinkLED, 250); // this sets the blink interval to 250ms
+var MOTOR = new Gpio(4, 'out') //use GPIO pin 4, change the 4 to respecify the pin number
+let blinkInterval = setInterval(blinkLED, 250); // this sets the blink interval to 250ms
 const express = require ('express');
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const oi = require("socket.io-client")
 
 
-//var socket = oi.connect("http://192.168.1.169:3456", {reconnect: true})
-//socket.on("FromServer", function(data){
-//console.log(data)
-//data ? (blinkInterval= setInterval(blinkLED, 250)) : blinkInterval = null 
-//})
-//console.log('hi')
-//let turnOnLED=false
+var socket = oi.connect("http://192.168.1.169:3456", {reconnect: true})
+socket.on("FromServer", function(data){
+console.log(data)
+data ? (writeSync(1)) : wirteSync(0)
+})
+console.log('Logging after socket fired off.')
 
 
-function blinkLED() { //function to start blinking
-    if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
-      LED.writeSync(1); //set pin state to 1 (turn LED on)
-    } else {
-      LED.writeSync(0); //set pin state to 0 (turn LED off)
-    }
-  }
+
+
+// function blinkLED() { //function to start blinking
+//     if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+//       LED.writeSync(1); //set pin state to 1 (turn LED on)
+//     } else {
+//       LED.writeSync(0); //set pin state to 0 (turn LED off)
+//     }
+//   }
   
-  function endBlink() { //function to stop blinking
-    clearInterval(blinkInterval); // Stop blink intervals
-    LED.writeSync(0); // Turn LED off
-    LED.unexport(); // Unexport GPIO to free resources
-  }
+  // function endBlink() { //function to stop blinking
+  //   clearInterval(blinkInterval); // Stop blink intervals
+  //   LED.writeSync(0); // Turn LED off
+  //   LED.unexport(); // Unexport GPIO to free resources
+  // }
   
-  setTimeout(endBlink, 10000); //stop blinking after 5 seconds
+  // setTimeout(endBlink, 10000); //stop blinking after 5 seconds
 
 
